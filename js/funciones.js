bd4b9d859bf606975490974d990f2b4ac604d2f8
Javier Llango <jallango1@espe.edu.ec>
2020-09-08 11:20:55 -0500

    Version2

--- js/funciones.js
+++ js/funciones.js
@@ -10,3 +10,28 @@ function sumar(){
     
 }
 
+function sumarPrimos(){
+    
+    var numero1=parseInt(document.getElementById('numero1').value);
+    var numero2=parseInt(document.getElementById('numero2').value);
+    
+            for (var i = 2; i < numero1; i++) {
+          
+              if (numero1 % i === 0) {
+                return console.log("no se puede sumar el numero 1");
+              }
+          
+            }
+    
+            for (var i = 2; i < numero2; i++) {
+          
+                if (numero2 % i === 0) {
+                  return console.log("no se puede sumar el numero 2");
+                }
+            
+              }
+          document.getElementById('resultado').innerHTML=numero1+numero2;
+           
+          
+           
+    }
