0eebb862c41e4f4e57e42b90a78fb5d968950fa9
Javier Llango <jallango1@espe.edu.ec>
2020-09-11 07:30:51 -0500

    Signed-off-by: Javier Llango <jallango1@espe.edu.ec> Version 1 area cuadrado

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,16 @@
 function limpiar(){
     document.getElementById('miFormulario').reset();
 }
-function sumar(){
+function areaCuadrado(){
     var valor1 = parseFloat(document.getElementById('valor1').value);
     var valor2 = parseFloat(document.getElementById('valor2').value);
     
-
-    document.getElementById('resultado').innerHTML = valor1+valor2;
+    if(valor1 == valor2){
+      document.getElementById('resultado').innerHTML = valor1+valor2;
+    }
+    else{
+      document.getElementById('resultado').innerHTML = "Los lados deben ser iguales";
+    }
     
 }
 
